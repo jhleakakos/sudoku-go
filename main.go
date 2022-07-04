428718b80c4f5b29c24979744c88bf387d60dfed
Joseph Leakakos <49359469+jhleakakos@users.noreply.github.com>
2022-07-03 19:01:09 -0600

    initial commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("| ---   ---   --- |")
+	fmt.Println("| 012 | 345 | 678 |")
+	fmt.Println("| 012 | 345 | 678 |")
+	fmt.Println("| 012 | 345 | 678 |")
+	fmt.Println("| --- | --- | --- |")
+	fmt.Println("| 012 | 345 | 678 |")
+	fmt.Println("| 012 | 345 | 678 |")
+	fmt.Println("| 012 | 345 | 678 |")
+	fmt.Println("| --- | --- | --- |")
+	fmt.Println("| 012 | 345 | 678 |")
+	fmt.Println("| 012 | 345 | 678 |")
+	fmt.Println("| 012 | 345 | 678 |")
+	fmt.Println("| ---   ---   --- |")
+}
