0263a35b154e6be9234de37742e01673dff5d1a1
Joseph Leakakos <49359469+jhleakakos@users.noreply.github.com>
2022-07-03 21:46:38 -0600

    move grid print to Board struct method

--- main.go
+++ main.go
@@ -1,19 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+type Board struct {
+	grid [9][9]int
+	//grid [9]int
+}
+
+func (b *Board) printBoard() {
+
+	fmt.Printf("%+v\n", b)
+	fmt.Println(b.grid)
+
+	fmt.Println("| ---   ---   --- |")
+	sb := ""
+	for i, n := range b.grid {
+		sb += "| "
+		for j, n := range n {
+			//fmt.Printf("j: %d, n: %d\n", j, n)
+			sb += strconv.Itoa(n)
+
+			if j == 2 || j == 5 {
+				sb += " | "
+			}
+		}
+		sb += " |\n"
+		if (i+1)%3 == 0 {
+			sb += "| --- | --- | --- |\n"
+		}
+	}
+
+	fmt.Print(sb)
+}
 
 func main() {
-	fmt.Println("| ---   ---   --- |")
-	fmt.Println("| 012 | 345 | 678 |")
-	fmt.Println("| 012 | 345 | 678 |")
-	fmt.Println("| 012 | 345 | 678 |")
-	fmt.Println("| --- | --- | --- |")
-	fmt.Println("| 012 | 345 | 678 |")
-	fmt.Println("| 012 | 345 | 678 |")
-	fmt.Println("| 012 | 345 | 678 |")
-	fmt.Println("| --- | --- | --- |")
-	fmt.Println("| 012 | 345 | 678 |")
-	fmt.Println("| 012 | 345 | 678 |")
-	fmt.Println("| 012 | 345 | 678 |")
-	fmt.Println("| ---   ---   --- |")
+
+	practiceBoard := Board{
+		grid: [9][9]int{
+			{0, 1, 2, 3, 4, 5, 6, 7, 8},
+			{0, 1, 2, 3, 4, 5, 6, 7, 8},
+			{0, 1, 2, 3, 4, 5, 6, 7, 8},
+			{0, 1, 2, 3, 4, 5, 6, 7, 8},
+			{0, 1, 2, 3, 4, 5, 6, 7, 8},
+			{0, 1, 2, 3, 4, 5, 6, 7, 8},
+			{0, 1, 2, 3, 4, 5, 6, 7, 8},
+			{0, 1, 2, 3, 4, 5, 6, 7, 8},
+			{0, 1, 2, 3, 4, 5, 6, 7, 8},
+		},
+	}
+	practiceBoard.printBoard()
+
 }
